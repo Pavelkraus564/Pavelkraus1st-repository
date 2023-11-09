d64a2ffe38114bbd172825b8d7a26084322e9a5d
Pavelkraus <pavelkraus07@gmail.com>
2023-11-09 11:43:22 +0100

    booga booga

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace returning_strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
